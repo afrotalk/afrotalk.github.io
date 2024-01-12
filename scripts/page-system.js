14ccb782659e447f29b91d63f80933df70169646
Shiloh Olotu <shiloholotu@gmail.com>
2024-01-11 18:54:20 -0600

    Style: update chat icon

--- scripts/page-system.js
+++ scripts/page-system.js
@@ -4,20 +4,27 @@ fadeCover.style["transition"] = (FADESPEED/1000) + "s";
 
 
 const pageIcons = {
-    "vocabPage":"navbarVocab",
-    "convoPage":"navbarConvo"
+    "vocabSelectPage":"navbarVocab",
+    "convoSelectPage":"navbarConvo"
 }
 
 function openPage(page){
     
     fadeCover.style["opacity"] = 1;
     fadeCover.style["pointer-events"] = "all";
+   
     setTimeout(function(){
         document.documentElement.scrollTop = 0;
         for(let i of document.getElementsByClassName("page")) i.style["display"] = "none";
         document.getElementById(page).style["display"] = "";
         fadeCover.style["opacity"] = 0;
         fadeCover.style["pointer-events"] = "none";
+
+        if(page in pageIcons){
+            for(j in pageIcons) document.getElementById(pageIcons[j]).style["background"] = "";
+            document.getElementById(pageIcons[page]).style["background"] = "var(--light-transp-blue)";
+            document.getElementById(pageIcons[page]).style["pointer-events"] = "var(--none)";
+        }
 
     }, FADESPEED);
     
